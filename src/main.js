4f4fc4b8e42d9d447b0b1874cb52245ab2f1ea6c
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2021-03-20 20:40:52 +0530

    init project

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
